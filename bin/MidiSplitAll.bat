f3dc6f437738d349234542eff8fbb6fe457364cb
gocha <gochaism@gmail.com>
2015-01-21 23:41:47 +0900

    Change Windows batch to kill REM prompts

--- bin/MidiSplitAll.bat
+++ bin/MidiSplitAll.bat
@@ -1,10 +1,10 @@
 @for %%a in (*.mid) do @midisplit "%%a" "%%~na-split.mid"
 
-rem @set midisplit_dir=%~dp0
-rem 
-rem :dispatch_loop
-rem   @if "%1"=="" @goto dispatch_done
-rem   @echo %midisplit_dir%midisplit "%1" "%~dpn1.mid"
-rem   @shift
-rem   @goto dispatch_loop
-rem :dispatch_done
+@rem @set midisplit_dir=%~dp0
+@rem 
+@rem :dispatch_loop
+@rem   @if "%1"=="" @goto dispatch_done
+@rem   @echo %midisplit_dir%midisplit "%1" "%~dpn1.mid"
+@rem   @shift
+@rem   @goto dispatch_loop
+@rem :dispatch_done
